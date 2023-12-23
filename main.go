caf09bd7580e6472c4178ad00b9116cb60963dcb
lezhou8 <dll145@protonmail.com>
2023-12-23 16:25:56 +1100

    use bubbles/list

--- main.go
+++ main.go
@@ -2,11 +2,63 @@ package main
 
 import (
 	"log"
+	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
+	// "github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/bubbles/list"
 )
 
+/* style */
+var style = lipgloss.NewStyle()
+// var italicsStyle = lipgloss.NewStyle().Italic(true)
+
+/* custom item */
+
+type File struct {
+	name string
+}
+
+func (f File) Title() string {
+	return f.name
+}
+
+func (f File) Description() string {
+	return ""
+}
+
+func (f File) FilterValue() string {
+	return f.name
+}
+
+/* main model */
+
 type Model struct {
+	list list.Model
+	err error
+}
+
+func New() *Model {
+	return &Model {}
+}
+
+func (m *Model) initList(w, h int) {
+	dirEntries, err := os.ReadDir(".")
+	if err != nil {
+		m.err = err
+		return
+	}
+
+	var items []list.Item
+	for _, dirEntry := range dirEntries {
+		items = append(items, File{ dirEntry.Name() })
+	}
+
+	m.list = list.New(items, list.NewDefaultDelegate(), w, h)
+
+	m.list.Title = "..."
+	m.list.SetShowHelp(false)
 }
 
 func (m Model) Init() tea.Cmd {
@@ -15,19 +67,17 @@ func (m Model) Init() tea.Cmd {
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-		case tea.KeyMsg:
-			switch msg.String() {
-				case "q", "ctrl+c":
-					return m, tea.Quit
-				case "up", "k":
-				case "down", "j":
-			}
+	case tea.WindowSizeMsg:
+		m.initList(msg.Width, msg.Height)
 	}
-	return m, nil
+
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
 }
 
 func (m Model) View() string {
-	return ""
+	return style.Render(m.list.View())
 }
 
 func main() {
@@ -37,8 +87,7 @@ func main() {
 	}
 	defer f.Close()
 
-	m := Model {
-	}
+	m := New()
 
 	p := tea.NewProgram(m)
 	if _, err := p.Run(); err != nil {
