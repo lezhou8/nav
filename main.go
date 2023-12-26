dec815046f6da2710187f0b08117737f5058c64c
lezhou8 <dll145@protonmail.com>
2023-12-26 16:01:49 +1100

    list refactor

--- main.go
+++ main.go
@@ -1,31 +1,65 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
 	// "github.com/charmbracelet/bubbles/key"
-	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/lipgloss"
 )
 
-/* style */
-var style = lipgloss.NewStyle()
-// var italicsStyle = lipgloss.NewStyle().Italic(true)
+/* rendering */
+
+type itemDelegate struct{}
+
+func (d itemDelegate) Height() int                               { return 1 }
+func (d itemDelegate) Spacing() int                              { return 0 }
+func (d itemDelegate) Update(msg tea.Msg, m *list.Model) tea.Cmd { return nil }
+
+func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	i, ok := listItem.(File)
+	if !ok {
+		return
+	}
+
+	str := string(i.name)
+	style := lipgloss.NewStyle().Padding(0, 2)
+
+	if i.fileType == dir {
+		style = style.Foreground(lipgloss.Color("12"))
+	} else if i.fileType == symlink {
+		style = style.Foreground(lipgloss.Color("32"))
+	}
+
+	if index == m.Index() {
+		style = style.Foreground(lipgloss.Color("11")).Bold(true)
+	}
+	if i.isSelected {
+		style = style.Italic(true)
+	}
+
+	fn := style.Render
+	fmt.Fprintf(w, fn(str))
+}
 
 /* custom item */
 
+type fileType uint
+
+const (
+	regular fileType = iota
+	dir
+	symlink
+)
+
 type File struct {
-	name string
-}
-
-func (f File) Title() string {
-	return f.name
-}
-
-func (f File) Description() string {
-	return ""
+	name       string
+	fileType   fileType
+	isSelected bool
 }
 
 func (f File) FilterValue() string {
@@ -36,14 +70,16 @@ func (f File) FilterValue() string {
 
 type Model struct {
 	list list.Model
-	err error
+	err  error
 }
 
 func New() *Model {
-	return &Model {}
+	var m Model
+	m.initList()
+	return &m
 }
 
-func (m *Model) initList(w, h int) {
+func (m *Model) initList() {
 	dirEntries, err := os.ReadDir(".")
 	if err != nil {
 		m.err = err
@@ -52,13 +88,30 @@ func (m *Model) initList(w, h int) {
 
 	var items []list.Item
 	for _, dirEntry := range dirEntries {
-		items = append(items, File{ dirEntry.Name() })
+		var f File
+		f.name = dirEntry.Name()
+		if dirEntry.IsDir() {
+			f.fileType = dir
+		} else if (dirEntry.Type() & os.ModeSymlink) != 0 {
+			f.fileType = symlink
+		} else {
+			f.fileType = regular
+		}
+		f.isSelected = false
+		items = append(items, f)
 	}
 
-	m.list = list.New(items, list.NewDefaultDelegate(), w, h)
-
-	m.list.Title = "..."
+	m.list = list.New(items, itemDelegate{}, 0, 0)
 	m.list.SetShowHelp(false)
+	m.list.SetShowStatusBar(false)
+	m.list.Styles.Title = lipgloss.NewStyle().Bold(true)
+
+	currDir, err := os.Getwd()
+	if err != nil {
+		m.list.Title = fmt.Sprintf("%s", err)
+		return
+	}
+	m.list.Title = string(currDir)
 }
 
 func (m Model) Init() tea.Cmd {
@@ -68,7 +121,8 @@ func (m Model) Init() tea.Cmd {
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
-		m.initList(msg.Width, msg.Height)
+		m.list.SetWidth(msg.Width)
+		m.list.SetHeight(msg.Height)
 	}
 
 	var cmd tea.Cmd
@@ -77,7 +131,7 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m Model) View() string {
-	return style.Render(m.list.View())
+	return m.list.View()
 }
 
 func main() {
